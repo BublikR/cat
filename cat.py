a70573fd57ee79a047df6d1d2f60246030e026e4
Roman Bublik <sir.bublik@gmail.com>
2019-10-17 01:34:29 +0300

    Add documentation line

--- cat.py
+++ cat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+r""" The implementation of the utility "cat" in python """
+
 import argparse, logging, sys
 
 __version__ = '0.0.1'
@@ -29,15 +31,10 @@ def main(args):
     parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument('-n', '--number', action='store_true', help='number all output lines')
     parser.add_argument('files', nargs='*', type=argparse.FileType('r'),default=[sys.stdin], metavar='FILE')
-    parser.add_argument('--run-test', action='store_true', help='run module tests')
     args = parser.parse_args(args)
 
-    if args.run_test:
-        import doctest
-        doctest.testmod()
-    else:
-        cat = Catter(args.files, args.number)
-        cat.run(sys.stdout)
+    cat = Catter(args.files, args.number)
+    cat.run(sys.stdout)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
