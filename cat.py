4028b5efd9a92f805153e7a6c03cdd66aba3fb18
Roman Bublik <sir.bublik@gmail.com>
2019-10-17 00:49:03 +0300

    Initial commit

---
+++ cat.py
@@ -0,0 +1,41 @@
+import argparse, logging, sys
+
+__version__ = '0.0.1'
+
+logging.basicConfig(level=logging.DEBUG)
+
+class Catter(object):
+
+    def __init__(self, files, show_numbers=False):
+        self.files = files
+        self.show_numbers = show_numbers
+
+    def run(self, fout):
+        fmt = '{0:>6} {1}'
+        for fin in self.files:
+            logging.debug('catting {0}'.format(fin))
+            for count, line in enumerate(fin, 1):
+                if self.show_numbers:
+                    fout.write(fmt.format(count, line))
+                else:
+                    fout.write(line)
+
+def main(args):
+    parser = argparse.ArgumentParser(description='Concatenate FILE(s), or ''standard input, to standard output')
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument('-n', '--number', action='store_true', help='number all output lines')
+    parser.add_argument('files', nargs='*', type=argparse.FileType('r'),default=[sys.stdin], metavar='FILE')
+    parser.add_argument('--run-test', action='store_true', help='run module tests')
+    args = parser.parse_args(args)
+
+    if args.run_test:
+        import doctest
+        doctest.testmod()
+    else:
+        cat = Catter(args.files, args.number)
+        cat.run(sys.stdout)
+        logging.debug('done catting')
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
